5ad9f5b39898c7b3fb703d13c9dad179fb426346
beat1886 <5a5qgixk@gmail.com>
2026-07-29 20:15:41 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-toastify/ReactToastify.min.css';
 import 'react-photo-view/dist/react-photo-view.css';
 import { GoogleAnalytics } from '@next/third-parties/google'
-// 引入底部组件
-import Footer from '@/components/Footer';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,18 +13,12 @@ export const metadata = {
   description: "图床",
 };
 
+
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="zh-CN">
-      <body className={inter.className}>
-        {/* 弹性布局，实现footer贴底 */}
-        <div className="min-h-screen flex flex-col">
-          <main className="flex-grow">
-            {children}
-          </main>
-          <Footer />
-        </div>
-      </body>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId="G-JVKEXR5XSG" />
     </html>
   );
